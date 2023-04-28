1df14d2351848c370af2288cbd8c1fd5094cf21a
acruz-23 <cruz.581@outlook.com>
2023-04-28 13:33:16 -0400

    added basic website structure, finsihed mortgage calculator page, started cd calculator page.

--- scripts/cd-calculator.js
+++ scripts/cd-calculator.js
@@ -15,7 +15,6 @@ function onMortCalcBtnClicked() {
   const interestRate =
     Number(document.getElementById("interestRate").value) / 100;
   const monthlyInterestRate = interestRate / 12;
-  console.log("monthly interest rate: " + monthlyInterestRate);
   const loanTerm = Number(document.getElementById("loanTerm").value);
   const monthlyLoanTerm = loanTerm * 12;
   const monthlyPayment =
@@ -26,4 +25,7 @@ function onMortCalcBtnClicked() {
     monthlyPayment.toFixed(2);
   document.getElementById("totalCost").innerHTML = loanTotal.toFixed(2);
   console.log("Mort Calc finsihed");
+  //   console.log("monthly interest rate: " + monthlyInterestRate);
+  // console.log("monthly Loan Term: " + monthlyLoanTerm);
+  //
 }
